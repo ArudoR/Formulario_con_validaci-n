c05693d4aff17d21c5c8739a982fdb8252387f7e
Aldo Mejia <aldonear@gmail.com>
2022-08-11 01:42:04 -0500

    Termino validacion si el formulario esta mal llenado

--- formulario.js
+++ formulario.js
@@ -117,5 +117,7 @@ formulario.addEventListener('submit', (e) => {
         document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
             icono.classList.remove('formulario__grupo-correcto');
         });
+    } else {
+        document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
     }
 });
